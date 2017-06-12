7101e3e8caec58562a0dbe77422704d0ac8323f3
mario <mario@digitalstate.ca>
2017-06-12 19:12:43 -0400

    Add development setup script

---
+++ development/setup.cmd
@@ -0,0 +1,25 @@
+@echo OFF
+
+for /F "tokens=1,2" %%i in (config.ini) do call :process %%i %%j
+
+cd "%~dp0"
+
+
+
+
+
+
+
+:process
+
+    set directory=%1
+    set repository=%2
+
+    if "%directory%" NEQ "" (
+        mkdir "%~dp0%directory%"
+        cd "%~dp0%directory%"
+        git clone "%repository%" .
+        docker-compose up -d
+    )
+
+goto :EOF
--- docker/rebuild.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-docker-compose stop "$1"
-docker-compose build "$1"
-docker-compose up -d --no-recreate "$1"
