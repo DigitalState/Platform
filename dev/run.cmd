27fbfcda06cdff5896a2b22731a5bfb5e0378f03
mario <mario@digitalstate.ca>
2017-08-31 22:04:22 -0400

    Restructure run commands

--- dev/run.cmd
+++ dev/run.cmd
@@ -11,27 +11,27 @@
 @echo OFF
 
 if %1 == "source:download" (
-    for /F "usebackq tokens=1,2 delims==" %%a in (config/src/dl.ini) do (
+    for /F "usebackq tokens=1,2 delims==" %%a in (config/source/download.ini) do (
         echo Downloading source for repository %%a
         call :source_download %%a %%b
     )
 ) else if %1 == "container:up" (
-    for /F "usebackq tokens=1,2 delims==" %%a in (config/ms/init.ini) do (
+    for /F "usebackq tokens=1,2 delims==" %%a in (config/container/up.ini) do (
         echo Upping container %%a
         call :container_up %%a %%b
     )
 ) else if %1 == "container:down" (
-    for /F "usebackq tokens=1,2 delims==" %%a in (config/ms/init.ini) do (
+    for /F "usebackq tokens=1,2 delims==" %%a in (config/container/down.ini) do (
         echo Downing container %%a
         call :container_down %%a %%b
     )
 ) else if %1 == "container:stop" (
-    for /F "usebackq tokens=1,2 delims==" %%a in (config/ms/init.ini) do (
+    for /F "usebackq tokens=1,2 delims==" %%a in (config/container/stop.ini) do (
         echo Stopping container %%a
         call :container_stop %%a %%b
     )
 ) else if %1 == "fixture:load" (
-    for /F "usebackq tokens=1,2 delims==" %%a in (config/ms/init.ini) do (
+    for /F "usebackq tokens=1,2 delims==" %%a in (config/fixture/load.ini) do (
         echo Loading fixture for %%a
         call :fixture_load %%a %%b
     )
