7416478a061b3e6cb0d9d279f7900c3edb3c6012
mario-digitalstate <mario@digitalstate.ca>
2018-09-11 14:49:31 -0400

    Add database migration and fixture behat contexts

---
+++ app/features/bootstrap/MigrationContext.php
@@ -0,0 +1,46 @@
+<?php
+
+use Behat\Behat\Context\Context;
+
+/**
+ * Class MigrationContext
+ */
+class MigrationContext implements Context
+{
+    /**
+     * @var array
+     */
+    protected $services;
+
+    /**
+     * Constructor
+     *
+     * @param array $services
+     */
+    public function __construct(array $services)
+    {
+        $this->services = $services;
+    }
+
+    /**
+     * @BeforeScenario @upMigrations
+     */
+    public function upMigrations()
+    {
+        foreach ($this->services as $service) {
+            shell_exec('cd /srv/'.$service.' && docker-compose exec -T php php bin/console doctrine:migrations:migrate --env=test --no-interaction');
+        }
+    }
+
+    /**
+     * Down migrations
+     *
+     * @AfterScenario @downMigrations
+     */
+    public function downMigrations()
+    {
+        foreach ($this->services as $service) {
+            shell_exec('cd /srv/'.$service.' && docker-compose exec -T php php bin/console doctrine:migrations:execute --env=test --no-interaction --down 1_0_0');
+        }
+    }
+}
